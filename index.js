aba213c13e2ae4890c0a4b36083829df9a37e7d5
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2024-01-12 15:47:56 -0600

    basic updates

--- index.js
+++ index.js
@@ -10,28 +10,9 @@ const formParametersElement = document.getElementById('formParameters');
 
 // Event Listeners
 
-electricInput.addEventListener('input', function () {
-    calculateTotal();
-});
-
-rentInput.addEventListener('input', function () {
-    calculateTotal();
-});
-
-gasInput.addEventListener('input', function () {
-    calculateTotal();
-});
-
-waterInput.addEventListener('input', function () {
-    calculateTotal();
-});
-
-phoneInput.addEventListener('input', function () {
-    calculateTotal();
-});
 
 submitButton.addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault();
     calculateTotal();
 });
 
@@ -44,20 +25,19 @@ function calculateTotal() {
     const waterAmount = parseFloat(waterInput.value) || 0;
     const phoneAmount = parseFloat(phoneInput.value) || 0;
 
+
+function displayIndividualAmount(billName, amount){
+    function displayIndividualAmount(billName, amount) {
+        formParametersElement.innerHTML += `<p><strong>${billName}:</strong> $${amount.toFixed(2)}</p>`;
+    }
+}
     formParametersElement.innerHTML = '';
-    displayIndividualAmount('Electric', electricAmount);
-    displayIndividualAmount('Rent', rentAmount);
-    displayIndividualAmount('Gas', gasAmount);
-    displayIndividualAmount('Water', waterAmount);
-    displayIndividualAmount('Phone', phoneAmount);
     displaytotalAmount('Total',electricAmount+rentAmount+gasAmount+phoneAmount+waterAmount)
 
-}
-
-function displayIndividualAmount(billName, amount) {
-    formParametersElement.innerHTML += `<p><strong>${billName}:</strong> $${amount.toFixed(2)}</p>`;
 }
 
 function displaytotalAmount(billName, amount) {
     formParametersElement.innerHTML +=`<p><strong>${billName}:</strong> $${amount.toFixed(2)}</p>`;
 }
+
+
