3dc1146ab756cdab1fd584b29b39b7079c9f0309
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2024-01-12 11:23:00 -0600

    updates to index.js and index.html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Variables
+
 const submitButton = document.querySelector('input[type="submit"]');
 const electricInput = document.querySelector('input[name="Electric"]');
 const rentInput = document.querySelector('input[name="Rent"]');
@@ -5,7 +7,45 @@ const gasInput = document.querySelector('input[name="Gas"]');
 const waterInput = document.querySelector('input[name="Water"]');
 const phoneInput = document.querySelector('input[name="Phone"]');
 
+// Event Listeners
+
 electricInput.addEventListener('input', function () {
-    const electricAmount = parseFloat(electricInput) || 0;  
-    calculatetotal();
+    const electricAmount = parseFloat(electricInput.value) || 0;  
+    calculateTotal();
 });
+
+rentInput.addEventListener('input', function () {
+    const rentAmount = parseFloat(rentInput.value) || 0;
+    calculateTotal();
+});
+
+gasInput.addEventListener('input', function () {
+    const gasAmount = parseFloat(gasInput.value) || 0;
+    calculateTotal();
+});
+
+waterInput.addEventListener('input', function () {
+    const waterAmount = parseFloat(waterInput.value) || 0;
+    calculateTotal();
+});
+
+phoneInput.addEventListener('input', function () {
+    const phoneAmount = parseFloat(phoneInput.value) || 0;
+    calculateTotal(phoneAmount);
+});
+
+submitButton.addEventListener('input',function() {
+    calculateTotal();
+})
+
+const newTotal = electricAmount + rentAmount + gasAmount + waterAmount + phoneAmount;
+    console.log('Updated Total Amount:', newTotal);
+
+    return newTotal;
+
+
+console.log('Electric:', electricAmount);
+console.log('Rent:', rentAmount);
+console.log('Gas:', gasAmount);
+console.log('Water:', waterAmount);
+console.log('Phone:', phoneAmount);
