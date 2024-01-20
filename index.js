962bef3e1b5e4f86e50dc35dd45e88a0857d0639
Alin Silcock <alinsilcock@Alin-Air.home>
2024-01-19 22:25:46 -0700

    updates

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+//add domcontentloader to call the fetch 
+
+
 // Variables
-const submitButton = document.querySelector('input[type="submit"]');
+const submitButton = document.querySelector('#sum-form');
 const submitFetchButton = document.getElementById("testButton")
-const electricInput = document.querySelector('input[name="Electric"]');
-const rentInput = document.querySelector('input[name="Rent"]');
-const gasInput = document.querySelector('input[name="Gas"]');
-const waterInput = document.querySelector('input[name="Water"]');
-const phoneInput = document.querySelector('input[name="Phone"]');
+const electricInput = document.getElementById("electricAmount");
+const rentInput = document.getElementById("rentAmount");
+const gasInput = document.getElementById("gasAmount");
+const waterInput = document.getElementById("waterAmount");
+const phoneInput = document.getElementById("phoneAmount");
 const formParametersElement = document.getElementById('formParameters');
 const electricBody = document.getElementById('electric-body');
 const gasBody = document.getElementById('gas-Body');
 const mouseMovement=document.getElementById('mouseMovement');
+let totalIsVisible = false;
 const headers = {
     "X-Params": {
         "frequency": "monthly",
@@ -65,38 +69,46 @@ const url = "https://api.eia.gov/v2/electricity/retail-sales/data/?api_key=DSoIe
 
 // Event Listeners
 //Gas
-const gasUrl= "https://api.eia.gov/v2/natural-gas/pri/rescom/data/?api_key=DSoIexmljF94PXO13LN5lyCmuRhsWNAI2BqGaA03&frequency=monthly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
+// const gasUrl= "https://api.eia.gov/v2/natural-gas/pri/rescom/data/?api_key=DSoIexmljF94PXO13LN5lyCmuRhsWNAI2BqGaA03&frequency=monthly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
 
 
 //Event Listener 
 
-submitButton.addEventListener('click', function (event) {
+submitButton.addEventListener('submit', (event) => {
     event.preventDefault();
     calculateTotal();
+    totalIsVisible = !totalIsVisible 
+    if (totalIsVisible){
+
+    } else {
+        formParametersElement.classList.add('hidden')
+    }
     console.log(submitButton)
 });
 
+let sumTotal = function() {
+    buttonOnOFF.style.display = 'block'
+}
 
-document.addEventListener('keydown', function(event) {
+
+document.addEventListener('keydown', (event) =>{
     if (event.key === 'Enter') {
-        event.preventDefault();
         calculateTotal();
         console.log(submitButton);
         console.log('Enter Key Pressed')
     }
 });
 
-window.addEventListener('scroll',function() {
+// window.addEventListener('scroll',function() {
    
-    console.log('page is being scrolled')
-};
+//     console.log('page is being scrolled')
+// };
 
 //The keydown event is fired when a key is pressed. Unlike the deprecated keypress event, 
 // the keydown event is fired for all keys, regardless of whether they produce a character value.
 
 
-submitFetchButton.addEventListener('click', function(event){
-    event.preventDefault(); console.log('clicked')
+submitFetchButton.addEventListener('click',(event)=>{
     fetch(url, {
         method: "GET",
         // headers: headers
@@ -109,8 +121,8 @@ submitFetchButton.addEventListener('click', function(event){
         //     Object.keys(item).forEach(key=>keys.add(key))
         // })
         const residentialData = response.response.data.filter(item => item['sectorName'] == 'residential')
-        console.log(`-----------------------`)
-        console.log(`Residential Data: ${JSON.stringify(residentialData)}`)
+        // console.log(`-----------------------`)
+        // console.log(`Residential Data: ${JSON.stringify(residentialData)}`)
         const rows = residentialData.map(item => {
             const tr = document.createElement('tr');
             tr.innerHTML = 
@@ -129,26 +141,26 @@ submitFetchButton.addEventListener('click', function(event){
         })
         });
 
-        const gasData = response.response.data.filter(item => item['sectorName'] == 'gas');
-        console.log(`-----------------------`);
-        console.log(`Gas Data: ${JSON.stringify(gasData)}`)
-        const gasRows = gasData.map(item => {
-            const tr = document.createElement('tr');
-            tr.innerHTML = 
-            `<td></td> 
-            <td>${item.period}</td>
-            <td>${item.stateid}</td>
-            <td>${item.stateDescription}</td>
-            <td>${item.period}</td>
-            <td>${item.sectorid}</td>
-            <td>${item.sectorName}</td>
-            <td>${item.price}</td>`;
-            return tr;
-        });
+//         const gasData = response.response.data.filter(item => item['sectorName'] == 'gas');
+//         console.log(`-----------------------`);
+//         console.log(`Gas Data: ${JSON.stringify(gasData)}`)
+//         const gasRows = gasData.map(item => {
+//             const tr = document.createElement('tr');
+//             tr.innerHTML = 
+//             `<td></td> 
+//             <td>${item.period}</td>
+//             <td>${item.stateid}</td>
+//             <td>${item.stateDescription}</td>
+//             <td>${item.period}</td>
+//             <td>${item.sectorid}</td>
+//             <td>${item.sectorName}</td>
+//             <td>${item.price}</td>`;
+//             return tr;
+//         });
 
-gasRows.forEach(row => {
-    gasBody.appendChild(row)
-})
+// gasRows.forEach(row => {
+//     gasBody.appendChild(row)
+// })
 });
 
 
@@ -203,5 +215,6 @@ function displayIndividualAmount(billName, amount){
 function displaytotalAmount(billName, amount) {
     formParametersElement.innerHTML +=`<p><strong>${billName}:</strong> $${amount.toFixed(2)}</p>`;
 }
+
 
 
